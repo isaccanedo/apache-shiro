5caafe5d82c78d1e00c2e61f67abf7fcc2e0467f
isaccanedo <isaccanedo@gmail.com>
2021-11-07 18:54:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Apache Shiro
+
+1. Visão Geral
+Neste artigo, veremos o Apache Shiro, uma estrutura de segurança Java versátil.
+
+O framework é altamente customizável e modular, pois oferece autenticação, autorização, criptografia e gerenciamento de sessão.
---
+++ src/test/java/com/isaccanedo/comparison/springsecurity/SpringContextTest.java
@@ -0,0 +1,19 @@
+package com.isaccanedo.comparison.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.isaccanedo.comparison.springsecurity.Application;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = { Application.class })
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+
+    }
+
+}
